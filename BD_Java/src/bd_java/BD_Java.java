0a4fbb44836ee8c5f72a3026793f31de07dcb0a8
Arthur Moura <arthurmoura.2007.ti@gmail.com>
2024-08-27 22:45:40 -0300

    Add files via upload

--- BD_Java/src/bd_java/BD_Java.java
+++ BD_Java/src/bd_java/BD_Java.java
@@ -1,28 +1,44 @@
+/**
+ *
+ * @author Arturo
+ * 
+ */
+
 package bd_java;
-import java.sql.*;
+import java.util.Scanner;
 
 public class BD_Java {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/bancodedados";
-        String username = "root";
-        String password = "";
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+        
+        var pesquisar = new pesquisar();
             
-            Connection con = DriverManager.getConnection(url, username, password);
-            
-            Statement st = con.createStatement();
-            
-            ResultSet resultSet = st.executeQuery( "select * from estudante");
-            
-            while(resultSet.next()){
-             System.out.println(resultSet.getInt(1) + " " + resultSet.getString(2) + resultSet.getInt(3) );
-            }
-            
-            con.close();
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Para efetuar a pesquisa siga o passo a passo a baixo de acordo com o que gostaria de pesquisar");
+        System.out.println("Digite 1 para pesquisar ID");
+        System.out.println("Digite 2 para pesquisar CPF");
+        System.out.println("Digite 3 para pesquisar NOME");
+        String metodoBusca = sc.nextLine();
+        
+        while(!metodoBusca.contentEquals("1") && !metodoBusca.contentEquals("2") && !metodoBusca.contentEquals("3")) {
+            System.out.println("Erro, tente novamente");
+            System.out.println("Digite 1 para pesquisar ID");
+            System.out.println("Digite 2 para pesquisar CPF");
+            System.out.println("Digite 3 para pesquisar NOME");
+            metodoBusca = sc.nextLine();
         }
-        catch(Exception e) {
-            System.out.println(e);
+        switch (metodoBusca) {
+            case "1" -> metodoBusca = "id";
+            case "2" -> metodoBusca = "cpf";
+            case "3" -> metodoBusca = "nome";
         }
+        
+        System.out.println("Qual " + metodoBusca +  " Gostaria de pesquisar?");
+        String itemBusca = sc.nextLine();
+        
+        
+        String resultadoPesquisa = pesquisar.pesquisar_tabela(metodoBusca, itemBusca);
+        
+        System.out.println(resultadoPesquisa);
     }
 }
